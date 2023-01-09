1784b0c7a8b39ba38d856d893d5bc1ce8a984f1c
Butros55 <geret.w@web.de>
2023-01-09 18:09:58 +0100

    changed some weapons sounds

--- Assets/scripts/Prefabs/Weapons/WeaponFire.cs
+++ Assets/scripts/Prefabs/Weapons/WeaponFire.cs
@@ -7,6 +7,8 @@ public class WeaponFire : MonoBehaviour
     public GameObject ParticelObject;
     public GameObject WeaponObject;
     public bool autoFire;
+    public float shootDelay;
+    private float counter;
 
     // Update is called once per frame
     void Update()
@@ -17,12 +19,20 @@ public class WeaponFire : MonoBehaviour
     void Shooting() { 
         if(autoFire) {
             if (Input.GetButton("Fire1")) {
-                Instantiate(ParticelObject, WeaponObject.transform.position, WeaponObject.transform.rotation);
+                counter -= Time.deltaTime;
+                if(counter <= 0) {
+                    Instantiate(ParticelObject, WeaponObject.transform.position, WeaponObject.transform.rotation);
+                    counter = shootDelay;
+                }
             }
         }
         else {
             if (Input.GetButtonDown("Fire1")) {
-                Instantiate(ParticelObject, WeaponObject.transform.position, WeaponObject.transform.rotation);
+                counter -= Time.deltaTime;
+                if(counter <= 0) {
+                    Instantiate(ParticelObject, WeaponObject.transform.position, WeaponObject.transform.rotation);
+                    counter = shootDelay;
+                }
             }
         }
     }
